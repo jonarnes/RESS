a5c1675be559ffeb972d08ba2545f9ef2e807499
Anders M. Andersen <anders.m.andersen@gmail.com>
2011-11-14 21:04:31 +0100

    removed RESS

--- util/RESS.php
+++ util/RESS.php
@@ -43,7 +43,7 @@
             // Set a cookie with the client side capabilities.
             var ccapDate = new Date()
             ccapDate.setFullYear(ccapDate.getFullYear() + 1);
-            d.cookie = 'RESS=' + RESS.ccap.vpw + extra + '; expires=' + ccapDate.toUTCString() + '; path=/;domain=.whateverweb.com';
+            d.cookie = 'RESS=' + vpw + extra + '; expires=' + ccapDate.toUTCString() + '; path=/;domain=.whateverweb.com';
         }
         //if (console && console.log) console.log('cookie: '+ d.cookie);
     }(window, document));
